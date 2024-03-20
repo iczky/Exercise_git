bf3df8c1307a6cc57fa2f7667af4476b6c4286a3
iczky <fiqra22@gmail.com>
2024-03-20 16:25:41 +0700

    feat: adding c1 to featureB

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("This is update inside master branch");
         System.out.println("Penambahan branch di repository");
         System.out.println("Untuk featureB");
+        System.out.println("C1 utk featureB");
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
